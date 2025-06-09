cd23f3777b38417a2289a194fa26e170144e3cd6
ibrahaik <ibrahim.haik.el.haouzi.mrabet@elpuig.xeill.net>
2025-06-09 16:02:05 +0200

    Actualización de repos.js

--- api/repos.js
+++ api/repos.js
@@ -1,10 +1,14 @@
+// api/repos.js
 export default async function handler(req, res) {
   const token = process.env.GITHUB_TOKEN;
+
   const response = await fetch('https://api.github.com/users/ibrahaik/repos', {
     headers: {
-      Authorization: `token ${token}`
+      Authorization: `token ${token}`,
+      'User-Agent': 'Vercel-App'
     }
   });
+
   const data = await response.json();
   res.status(200).json(data);
 }
